dbb1e668ceb7b96fd3df8b42d2c71554a12e8676
David <zjdavid.2003@gmail.com>
2018-01-28 14:23:49 -0500

    Initial commit

---
+++ app/src/main/java/org/dwei/mfsmobile/MainActivity.kt
@@ -0,0 +1,28 @@
+package org.dwei.mfsmobile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.content.Intent
+
+
+
+class MainActivity : AppCompatActivity() {
+    val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button  */
+    fun sendMessage(view: View) {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<View>(R.id.editText) as EditText
+        val message = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
+        // Do something in response to button
+    }
+}
